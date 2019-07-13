01675123d7bbbf49e17a1c599b61c12062ec982a
nursyahidke <52354190+nursyahidke@users.noreply.github.com>
2019-07-13 22:10:45 +0800

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,13 +1,3 @@
-/*function segitiga() {
-	var nilai = 5;
-	for(a = 1; a <= nilai; a++) {
-		for(b = 1; b <= a; b++) {
-			document.write(b+" ");
-  		}
- 		document.write("\n");
- 	}
-}
-*/
 function segitiga() {
 	var row = 5;
 	for (var i = 1; i <= row; i++) {
