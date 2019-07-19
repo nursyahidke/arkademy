5b046b6d91f132e5a82083c4c18ec397c6609764
nursyahidke <52354190+nursyahidke@users.noreply.github.com>
2019-07-19 20:28:00 +0800

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,12 +1,39 @@
-function segitiga() {
-	var row = 5;
-	for (var i = 1; i <= row; i++) {
-		var result = '';
-		for (var j = 1; j <= i; j++) {
-	    	result += j;
-		}
-		console.log(result);
-	}
+let number = 7;
+triangle(number);
+
+// triangle(5);
+
+function triangle(num){
+    if(0 < num < 10){
+        let res = '';
+        let x = 2;
+        for(let i=1; i<=num; i++){
+            for(let j=0; j<i; j++){
+                while(isPrime(x) == false){
+                    x++;
+                }
+                res = res+ ' ' +x;
+                x++;
+            }
+            console.log(res);
+            res = '';
+        }
+    }else{
+        console.log("error");
+    }
 }
 
-segitiga();
+function isPrime(n){
+    if (n===1){
+      return false;
+    }else if(n === 2){
+        return true;
+    }else{
+        for(var x = 2; x < n; x++){
+            if(n % x === 0){
+            return false;
+            }
+        }
+        return true;
+    }
+}
