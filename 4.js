46d70243d4c1cff405ad1946ab72cc8ff9a50e4f
nursyahidke <52354190+nursyahidke@users.noreply.github.com>
2019-07-19 20:27:06 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,21 +1,52 @@
-function beliTelur(tgl, uang) {
-	var telur = uang/2000;
-	var totalTelur;
-	if (tgl%2!=0 && tgl%5==0){
-		if (telur >= 12 && telur <= 20){
-			totalTelur = telur + (1 * 5);
-			console.log(totalTelur); 
-		}else if (telur >= 20 && telur < 40){
-			totalTelur = telur + (3 * 5)
-			console.log(totalTelur);
-		}else{
-			totalTelur = telur + (6 * 10);
-			console.log(totalTelur);
-		}
-	}else{
-		totalTelur = telur;
-		console.log(totalTelur);
-	}
+// buyEgg(25, 50000);
+// buyEgg(100, 100000);
+
+function buyEgg(date, money){
+    let price = 2000;
+    let egg = Math.floor(money/price)
+    if(date % 2 == 1){
+        bonus = (Math.floor(egg/20)) * 3;
+        if(isPrime(date)){
+            bonus = bonus + Math.floor(egg/12);
+            if(date % 5 == 0){
+                if(bonus % 2 == 0){
+                    egg = egg + (bonus*10);
+                }else{
+                    egg = egg + (bonus*5);
+                }
+                console.log(egg);
+            }
+        }else{
+            if(date % 5 == 0){
+                if(bonus % 2 == 0){
+                    egg = egg + (bonus*10);
+                }else{
+                    egg = egg + (bonus*5);
+                }
+                console.log(egg);
+            }
+        }
+    }else{
+        if(isPrime(date)){
+            egg = egg + Math.floor(bonus/12);
+        }
+        console.log(egg);
+    }
 }
 
-beliTelur(25, 50000);
+function isPrime(num){
+    if (num===1){
+        return false;
+      }else if(num === 2){
+          return true;
+      }else{
+          for(var x = 2; x < num; x++){
+              if(num % x === 0){
+              return false;
+              }
+          }
+          return true;
+      }
+}
+
+buyEgg(5, 150000);
